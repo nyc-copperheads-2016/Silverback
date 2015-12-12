3246252778bd4355375d0169385b30b953d0383c
Ali Lyons & Jonathan Chen <pair+arlyons.dijonkitchen@devbootcamp.com>
2015-12-12 16:26:29 -0500

    Fix working login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ post '/sessions' do
   user = User.find_by(username: params[:username])
   if user && user.password == params[:password]
     session[:user_id] = user.id
-    redirect '/'
+    redirect '/surveys'
   else
     redirect '/?errors=incorrect_user_or_password'
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ActiveRecord::Base
   has_and_belongs_to_many :choices
   has_many :authored_surveys, class_name: :Survey
 
-  validates :password_confirmation, length: { minimum: 5}
-  validates_confirmation_of :password, :confirm => :password_confirmation
+  # validates :password_confirmation, length: { minimum: 5}
+  # validates_confirmation_of :password, :confirm => :password_confirmation
 
-  def password_confirmation_matches
-    unless password_confirmation == self.password
-      errors.add(:password_confirmation, "must be a match")
-    end
-  end
+  # def password_confirmation_matches
+  #   unless password_confirmation == self.password
+  #     errors.add(:password_confirmation, "must be a match")
+  #   end
+  # end
 
   def password
     @password ||= Password.new(password_hash)
