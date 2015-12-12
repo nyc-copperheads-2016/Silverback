60cf403203b84296cbd7bf65e4aa68834aeae42c
Aaron Tsai <email": null>
2015-12-12 14:53:57 -0500

    Add surveys controller and semantic ui styling

---
+++ app/controllers/surveys_controller.rb
@@ -0,0 +1,14 @@
+get '/surveys' do
+  surveys = Survey.order(created_at: :desc)
+  erb :'/surveys/index', locals: {surveys: surveys}
+end
+
+get '/surveys/new' do
+  erb :'surveys/form'
+end
+
+
+post '/surveys' do
+
+
+end
