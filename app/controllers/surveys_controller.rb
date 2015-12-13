8f0404dc6c6b78b30eb230ffbe51bb8f34824519
arlyons <lyons500@gmail.com>
2015-12-12 21:10:58 -0500

    Not able to properly redirect

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -4,34 +4,38 @@ get '/surveys' do
 end
 
 get '/surveys/new' do
-  erb :'surveys/form'
+  erb :'/surveys/form_survey_title'
 end
-
 
 post '/surveys' do
   survey = Survey.new(title: params[:title], user_id: current_user.id)
   current_user.surveys << survey
-  question = Question.new(question_text: params[:question])
-  survey.questions << question
 
-  choice_parser(params[:choice]).each do |c|
-    choice = Choice.create!(choice_text: c)
-    question.choices << choice
+  if survey.save
+    redirect '/surveys/:id/questions/:id'
   end
+  # erb :'/surveys/form_survey_question_choices'
+  # question = Question.new(question_text: params[:question])
+  # survey.questions << question
 
-  if survey.save && question.save
-    redirect '/'
-  else
-    @errors = survey.errors.full_messages
-    erb :'surveys/form'
-  end
+  # choice_parser(params[:choice]).each do |c|
+  #   choice = Choice.create!(choice_text: c)
+  #   question.choices << choice
+  # end
+
+  # if survey.save && question.save
+  #   redirect '/'
+  # else
+  #   @errors = survey.errors.full_messages
+  #   erb :'surveys/form_survey_title'
+  # end
 
 end
 
 
 get '/surveys/:id' do
   survey = Survey.find_by(id: params[:id])
-  erb :'surveys/show', locals: {survey: survey}
+  erb :'/surveys/show', locals: {survey: survey}
 end
 
 post '/surveys/:id' do
